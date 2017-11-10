fc3b67322317812be54eb17b455e60345730e863
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2017-11-10 18:33:33 +0100

    Updated scripts

--- 3.4.10/include/opt/zookeeper/bin/zkOk.sh
+++ 3.4.10/include/opt/zookeeper/bin/zkOk.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ZOOKEEPER_CLIENT_PORT=${ZOOKEEPER_CLIENT_PORT:-2181}
-OK=$(echo ruok | nc 127.0.0.1 $ZOOKEEPER_CLIENT_PORT)
+OK=$(echo ruok | nc localhost $ZOOKEEPER_CLIENT_PORT)
 if [ "$OK" == "imok" ]; then
 	exit 0
 else
